56460e82c1701e2a14b47606a78c3d46ffb76f48
Juls00 <juliagimena00@alumnos.cei.es>
2025-10-28 11:34:59 +0100

    up

---
+++ syntax/ej1.js
@@ -0,0 +1,10 @@
+/**
+ * Ejercicio 1. Crear un array llamado meses y que almacene el nombre de los doce meses del año. 
+ * Mostrar por
+ * pantalla los doce nombres utilizando console.log().
+ */
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+// Se usa un forEach para imprimir uno a uno 
+console.log(meses);
